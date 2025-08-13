ed0523ae0dc012de35a2168b429793804cef10ff
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-08-13 12:37:56 +0300

    feat(course-player): add course player component with video playback and lesson navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { Courses } from './components/courses/courses';
 import { Certificates } from './components/student/certificates/certificates';
 import { CourseDetails } from './components/courses/course-details/course-details';
 import { CourseVideoPlayer } from './components/courses/course-video-player/course-video-player';
+import { CoursePlayer } from './components/courses/course-player/course-player';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -21,6 +22,7 @@ export const routes: Routes = [
   },
   { path: 'coursedetails/:id', component: CourseDetails },
   { path: 'coursevideo/:id', component: CourseVideoPlayer },
+  { path: 'courseplayer/:id', component: CoursePlayer },
   { path: 'certificates', component: Certificates },
   { path: '**', component: Notfound },
 ];
---
+++ src/app/components/courses/course-player/course-player.ts
@@ -0,0 +1,91 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+type Lesson = { title: string; time: string; url: string; preview?: boolean };
+type Section = { section: string; duration: string; lessons: Lesson[] };
+
+@Component({
+  selector: 'app-course-player',
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './course-player.html',
+  styleUrls: ['./course-player.css']
+})
+export class CoursePlayer{
+  course: any;
+  expanded = new Set<number>();
+  currentLesson?: Lesson;
+  progress = 0;
+
+  private courses = [
+    {
+      id: 1,
+      title: 'Mastering Angular for Beginners',
+      breadcrumb: ['My Courses', 'Angular'],
+      lessons: 38,
+      duration: '4h 30m',
+      rating: 5,
+      reviews: 126,
+      description:
+        'This is your enrolled course. Continue watching from where you left off.',
+      content: <Section[]>[
+        {
+          section: '01: Intro',
+          duration: '22min',
+          lessons: [
+            {
+              title: 'Introduction',
+              time: '2 min',
+              url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+            },
+            {
+              title: 'What is Angular?',
+              time: '5 min',
+              url: 'https://www.w3schools.com/html/movie.mp4',
+            },
+          ],
+        },
+        {
+          section: '02: Intermediate',
+          duration: '1h 20m',
+          lessons: [
+            {
+              title: 'Signals vs RxJS',
+              time: '14 min',
+              url: 'https://www.w3schools.com/html/mov_bbb.mp4',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.course = this.courses.find((c) => c.id === id) ?? this.courses[0];
+    this.expanded.add(0);
+    this.currentLesson = this.course.content[0].lessons[0];
+    this.updateProgress();
+  }
+
+  toggleSection(i: number) {
+    this.expanded.has(i) ? this.expanded.delete(i) : this.expanded.add(i);
+  }
+
+  isOpen(i: number) {
+    return this.expanded.has(i);
+  }
+
+  playLesson(lesson: Lesson) {
+    this.currentLesson = lesson;
+    this.updateProgress();
+  }
+
+  updateProgress() {
+    const allLessons = this.course.content.flatMap((s: { lessons: any; }) => s.lessons);
+    const index = allLessons.findIndex((l: any) => l === this.currentLesson);
+    this.progress = ((index + 1) / allLessons.length) * 100;
+  }
+}
